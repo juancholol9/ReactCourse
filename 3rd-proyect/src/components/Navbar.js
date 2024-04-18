48ab401e93aa91dd78d3fe72ca8073eb71a5b2df
juancholol9 <juanramonmejiapadilla@gmail.com>
2024-04-18 15:09:54 -0600

    Deleted defaulf React Apps
    Edited html, added index.js file and style.css, minor changes donde to the js files. Reseted html styling using '*' wildcard in css

---
+++ 3rd-proyect/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnbLogo from "../images/airbnb-logo.jpeg;"
+
+export default function Navbar(){
+    return(
+        <div>
+            <img src={airbnbLogo}></img>
+        </div>
+    )
+}
