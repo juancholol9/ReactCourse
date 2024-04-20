1270247a83ed64f46fb3300469c3136cd84594b9
juancholol9 <juanramonmejiapadilla@gmail.com>
2024-04-20 14:27:49 -0600

    AirBnb clone Proyect done
    Added small changes to the code such as the badge on every image

--- 3rd-proyect/src/App.js
+++ 3rd-proyect/src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App(){
+
+    const cards = data.map(item => {
+        return(
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
     return(
         <div className="main">
             <Navbar/>
             <Hero/>
             <div className="main--card">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {cards}
             </div>
         </div>
     )
--- 3rd-proyect/src/components/Card.js
+++ 3rd-proyect/src/components/Card.js
@@ -2,21 +2,30 @@ import React from "react";
 import img from "../images/Katie.png"
 import imgStar from "../images/star.png"
 
-export default function Card(){
+export default function Card(props){
+    var badgeText
+    if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return(
-        <div class="card">
-            <img class="card--image" src={img}/>
-            <div class="card--stats">
+        <div className="card">
+            <div>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            </div>
+            <img className="card--image" src={img}/>
+            <div className="card--stats">
                 <img src={imgStar}/>
-                <span>5.0</span>
-                <span>(6)</span>
-                <span>USA</span>
+                <span>{props.stats.rating} •</span>
+                <span>({props.stats.reviewCount}) </span>
+                <span>{props.location}</span>
             </div>
             <div>
-                <p>Life Lessons with Katie Zaferes</p>
-                <p><span className="bold">From $136</span> / person</p>
+                <p>{props.title}</p>
+                <p><span className="bold">From ${props.price}</span> / person</p>
             </div>
-
         </div>
     )
 }
